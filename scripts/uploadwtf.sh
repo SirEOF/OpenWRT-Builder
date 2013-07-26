a8bff9e127edb3300f3626b621984fbfae90c643
Tiago Peralta <tiagof@amazon.com>
2013-07-26 22:05:14 +0000

    Removed some packages from rootfs modified upload script (still not working automatically cuz im lazy)

--- scripts/uploadwtf.sh
+++ scripts/uploadwtf.sh
@@ -1,1 +1,1 @@
-lftp -e 'set ftp:passive-mode false; set net:timeout 10; mirror -R '/home/ubuntu/Sources/stable'/bin/x86_64/ /home/tiagoperalta/domains/ec2-openwrt.tiagoperalta.pt/public_html/12.09.1-EC2-x86_64/' -u tiagoperalta,Maverick1! ec2-openwrt.tiagoperalta.pt
+lftp -e 'set ftp:passive-mode false; set net:timeout 10; mirror -R '/home/ubuntu/Sources/stable-amd64'/bin/x86_64/ /home/tiagoperalta/domains/ec2-openwrt.tiagoperalta.pt/public_html/12.09.1-EC2-x86_64/' -u tiagoperalta,Maverick1! ec2-openwrt.tiagoperalta.pt
